4a2c9f177b303aab8769f130d15b9691d77b93a7
jordanbreen28 <jordan.breen@perforce.com>
2023-07-04 16:02:03 +0100

    refactor: Refactor PuppetCommit class in lib/puppet-commit.rb

--- lib/puppet-commit.rb
+++ lib/puppet-commit.rb
@@ -17,14 +17,10 @@ class PuppetCommit
       docs: 'Documentation only changes',
       style: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
       refactor: 'A code change that neither fixes a bug nor adds a feature',
-      perf: 'A code change that improves performance',
-      test: 'Adding missing tests or correcting existing tests',
-      build: 'Changes that affect the build system or external dependencies',
-      ci: 'Changes to our CI configuration files and scripts',
-      chore: "Other changes that don't modify src or test files",
+      maint: "Other changes that don't modify src or test files",
       revert: 'Reverts a previous commit',
       feat: 'A new feature',
-      fix: 'A bug fix'
+      bugfix: 'A bug fix'
     }
 
     command = 'generate a concise commit messgage in the present tense, based on the git diff supplied at the end of this message.' \
