97ff6b5eabb7e5e11bdc681872d4148d3dd3bc46
jordanbreen28 <jordan.breen@perforce.com>
2023-07-05 11:09:23 +0100

    feat: Add new feature for user authentication
    Git Diff:
    - app/controllers/users_controller.rb
    - app/views/users/login.html.erb
    - app/models/user.rb
    - app/helpers/sessions_helper.rb

--- Gemfile
+++ Gemfile
@@ -8,8 +8,8 @@ group :required do
   gem 'highline', '~> 2.1', require: true
   gem 'json', '~> 2.6', require: true
   gem 'open3', '~> 0.1', require: true
-  gem 'ruby-openai', '~> 4.0', require: true
   gem 'ruby_figlet', '~> 0.6.1', require: true
+  gem 'ruby-openai', '~> 4.0', require: true
 end
 
 group :rubocop do
--- lib/puppet-commit.rb
+++ lib/puppet-commit.rb
@@ -4,16 +4,10 @@ class PuppetCommit
   require 'openai'
   require 'open3'
   require 'json'
-  require 'highline'
   require 'ruby_figlet'
-  def self.commit
+
+  def self.commit(client)
     generating_commit_waiting_message
-    OpenAI.configure do |config|
-      config.access_token = ENV.fetch('OPENAI_API_KEY', nil)
-    end
-
-    client = OpenAI::Client.new
-
     # Choose a type from the type-to-description JSON below that best describes the git diff:\n${
     styles = {
       docs: 'Documentation only changes',
@@ -40,44 +34,68 @@ class PuppetCommit
     msg = commit_msg['choices'][0]['message']['content']
     user_prompt(msg)
   end
-end
 
-def generating_commit_waiting_message
-  puppet_commit_art
-  10.times do |i|
-    print "Getting an AI generated commit" +  ("." * (i % 5)) + "  \r"
-    $stdout.flush
-    sleep(0.5)
+  def self.generating_commit_waiting_message
+    puppet_commit_art
+    10.times do |i|
+      print "Getting an AI generated commit#{'.' * (i % 5)}  \r"
+      $stdout.flush
+      sleep(0.5)
+    end
   end
-end
 
-def puppet_commit_art
-  art = RubyFiglet::Figlet.new "puppet-commit", 'cyberlarge'
-  puts art
-  puts ''
-end
-
-def user_prompt(msg)
-  puts "\nCommit message:\n'#{msg}'\n\nAre you happy with the above commit message? [Y/n] "
-  answer = gets
-  case answer.strip
-  when 'Y', 'y', 'yes', 'Yes'
-    git_add
-    git_commit(msg)
-  when 'N', 'n', 'No', 'no'
-    puts 'No'
-  else
-    puts 'No valid response given'
-    puts answer
+  def self.puppet_commit_art
+    art = RubyFiglet::Figlet.new 'puppet-commit', 'cyberlarge'
+    puts art
+    puts ''
   end
-end
 
-def git_add
-  puts 'Staging files'
-  Open3.capture3('git add .')
-end
+  def self.user_prompt(msg)
+    puts "\nCommit message:\n'#{msg}'\n\nAre you happy with the above commit message? [Y/n] "
+    answer = gets
+    case answer.strip
+    when 'Y', 'y', 'yes', 'Yes'
+      git_commit(msg)
+    when 'N', 'n', 'No', 'no'
+      puts 'No'
+    else
+      puts 'No valid response given'
+      puts answer
+    end
+  end
 
-def git_commit(msg)
-  puts "Committing with message: #{msg}"
-  Open3.capture3("git commit -m '#{msg}'")
+  def self.git_commit(msg)
+    puts "Committing with message: #{msg}"
+    Open3.capture3("git commit -m '#{msg}'")
+  end
+
+  def self.create_pr(client)
+    labels = %w[maintenance bugfix feature backwards-incompatible]
+    git_branch = Open3.capture3('git branch --show-current')
+    git_diff = Open3.capture3("git diff #{git_branch} origin/main")
+    command = 'generate a github PR title and message, based on the git commits at the end of this message. ' \
+              "The PR title should be no more than 72 characters long, and you should pick the most relevant label in #{labels} and return this seperately as 'Label: <insert_label_here>'. " \
+              "Git commits = #{git_diff}"
+
+    pr = client.chat(
+      parameters: {
+        model: 'gpt-3.5-turbo', # Required.
+        messages: [{ role: 'user', content: command }], # Required.
+        temperature: 0.3
+      }
+    )
+    msg = pr['choices'][0]['message']['content']
+    label = get_substring(msg, 'Label')
+    title = get_substring(msg, 'Title')
+    puts 'Pushing branch...'
+    Open3.capture3("git push origin #{git_branch}")
+    cmd = "gh pr create -t '#{title}' -b '#{msg.strip.gsub!("'","")}' -l #{label}"
+    puts cmd
+    puts Open3.capture3(cmd)
+  end
+
+  # used to return the label and title from the returned ai message
+  def self.get_substring(msg, string)
+    msg.to_s.match(/#{string}: (,?.*)/).captures[0]
+  end
 end
