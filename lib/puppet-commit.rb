1c27f107597c3596fe7f6f42ceefb5f348f2a7cd
jordanbreen28 <jordan.breen@perforce.com>
2023-07-05 08:21:26 +0100

    refactor PuppetCommit class to modify source code style

--- lib/puppet-commit.rb
+++ lib/puppet-commit.rb
@@ -16,8 +16,7 @@ class PuppetCommit
     styles = {
       docs: 'Documentation only changes',
       style: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-      refactor: 'A code change that neither fixes a bug nor adds a feature',
-      maint: "Other changes that don't modify src or test files",
+      maint: "Other changes that don't modify source code",
       revert: 'Reverts a previous commit',
       feat: 'A new feature',
       bugfix: 'A bug fix'
